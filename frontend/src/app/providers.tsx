2efae4d28c02cfe2903e40edae3ed5ff3ada0e02
Karthik7411 <karthik648464@gmail.com>
2026-08-11 17:38:06 +0530

    updated front end

--- frontend/src/app/providers.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import React from "react";
-import { ToastProvider } from "@/components/Toast";
-
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <ToastProvider>{children}</ToastProvider>;
-}
