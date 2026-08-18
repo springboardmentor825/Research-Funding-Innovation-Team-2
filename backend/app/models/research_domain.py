012494b26873cc6cec753ef9a4a63a721fcd22d8
Pratik Kumar <pratikkumar5669@gmail.com>
2026-08-18 18:10:35 +0530

    Research domain Api's updated

---
+++ backend/app/models/research_domain.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+from bson import ObjectId
+
+
+class ResearchDomainModel:
+    @staticmethod
+    def create_document(data: dict) -> dict:
+        now = datetime.utcnow()
+
+        return {
+            "_id": data.get("_id", ObjectId()),
+            "name": data["name"],
+            "code": data["code"],
+            "description": data.get("description"),
+            "created_at": data.get("created_at", now),
+            "updated_at": data.get("updated_at", now),
+        }
