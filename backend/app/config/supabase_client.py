91691cd99442cfa7bc868c4b480fee63a6d3a14b
Karthik7411 <karthik648464@gmail.com>
2026-09-17 01:07:16 +0530

    Keep frontend only

--- backend/app/config/supabase_client.py
+++
@@ -1,7 +0,0 @@
-from supabase import create_client, Client
-from app.config.settings import SUPABASE_URL, SUPABASE_SECRET_KEY
-
-supabase: Client = create_client(
-    SUPABASE_URL,
-    SUPABASE_SECRET_KEY
-)
